d5bded6e4d04fb1802f0a097c7534341bff6ab4b
LG <git@LG>
2023-11-28 18:15:59 +0700

    init getIcpCommand 2023-11-28.181555

--- src/main/java/net/ysh/nemoo/command/NemooCommand.java
+++ src/main/java/net/ysh/nemoo/command/NemooCommand.java
@@ -1,12 +1,6 @@
 package net.ysh.nemoo.command;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
-import net.ysh.nemoo.bean.BajoInput;
-import net.ysh.nemoo.bean.BajoOutput;
-
 public interface NemooCommand {
 
+	public String getIcpCommand() throws Exception;
 }
--- src/main/java/net/ysh/nemoo/controller/NemooController.java
+++ src/main/java/net/ysh/nemoo/controller/NemooController.java
@@ -1,10 +1,16 @@
 package net.ysh.nemoo.controller;
 
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.ysh.nemoo.bean.icp.IcpNemooController;
+import net.ysh.nemoo.bean.icp.IcpNemooProxy;
 import net.ysh.nemoo.command.NemooCommand;
+import net.ysh.nemoo.service.NemooService;
 import net.ysh.rest.YshRestConstant;
 import net.ysh.rest.command.YshCommand;
 
@@ -12,10 +18,22 @@ import net.ysh.rest.command.YshCommand;
 @RequestMapping(YshRestConstant.REST + YshRestConstant.V1)
 public class NemooController implements NemooCommand, YshCommand {
 
+	@Autowired
+	private NemooService nemooService;
+
+	@Autowired
+	private IcpNemooController nemooController;
+
 	@Override
 	@GetMapping(value = YshRestConstant.COMMAND)
 	public String getCommand() {
 		return getClass().getName();
+	}
+
+	@Override
+	@GetMapping(value = "getIcpCommand")
+	public String getIcpCommand() throws Exception {
+		return nemooController.getCommand();
 	}
 
 }
